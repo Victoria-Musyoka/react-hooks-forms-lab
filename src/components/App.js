3103b8c77f4ad54d8b00537168ddd83d384b9c72
Victoria Musyoka <victoria.musyoka@student.moringaschool.com>
2022-07-20 13:37:06 +0300

    complete

--- src/components/App.js
+++ src/components/App.js
@@ -1,3 +1,5 @@
+
+
 import React, { useState } from "react";
 import ShoppingList from "./ShoppingList";
 import Header from "./Header";
@@ -14,7 +16,7 @@ function App() {
   return (
     <div className={"App " + (isDarkMode ? "dark" : "light")}>
       <Header isDarkMode={isDarkMode} onDarkModeClick={handleDarkModeClick} />
-      <ShoppingList items={items} />
+      <ShoppingList items={items} setItems={setItems}/>
     </div>
   );
 }
--- src/components/ItemForm.js
+++ src/components/ItemForm.js
@@ -1,24 +1,41 @@
-import React from "react";
+import React, { useState } from "react";
 import { v4 as uuid } from "uuid";
 
-function ItemForm(props) {
+function ItemForm({onItemFormSubmit}) {
+  const [itemName, setItemName] = useState("");
+  const [itemCategory, setItemCategory] = useState("Produce")
+
+  function handleItemChange(event){
+    setItemName(event.target.value);
+  }
+  function handleCategoryChange(event){
+    setItemCategory(event.target.value);
+  }
+  function handleOnSubmit(event){
+    event.preventDefault();
+    onItemFormSubmit({
+      id: uuid(),
+      name: itemName,
+      category: itemCategory,
+    });
+  }
   return (
     <form className="NewItem">
       <label>
         Name:
-        <input type="text" name="name" />
+        <input type="text" name="name" onChange={handleItemChange} value={itemName}/>
       </label>
 
       <label>
         Category:
-        <select name="category">
+        <select name="category" onChange={handleCategoryChange} value={itemCategory}>
           <option value="Produce">Produce</option>
           <option value="Dairy">Dairy</option>
           <option value="Dessert">Dessert</option>
         </select>
       </label>
 
-      <button type="submit">Add to List</button>
+      <button type="submit" onSubmit={handleOnSubmit}>Add to List</button>
     </form>
   );
 }
